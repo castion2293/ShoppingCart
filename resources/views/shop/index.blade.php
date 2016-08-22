ae781182940ab7f3a6452f88aff58360ca90de42
castion2293 <castion2293@yahoo.com.tw>
2016-08-22 17:39:00 -0600

    Product Data

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,19 +5,23 @@
 @endsection
 
 @section('content')
-   <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-            <img src="https://img.buzzfeed.com/buzzfeed-static/static/enhanced/webdr02/2013/7/31/12/grid-cell-12788-1375287846-44.jpg" alt="...">
-            <div class="caption">
-                <h3>Product Title</h3>
-                <p class="description">lorem akjskfkdsaj a fjkadsjfldjafjdslafjdflk d lkafjdafjdslkfjslaj kaljfkdjfdsi</p>
-                <div class="clearfix">
-                  <div class="pull-left price">$12</div>
-                  <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+   @foreach($products->chunk(3) as $productChunk)
+         <div class="row">
+            @foreach($productChunk as $product)
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                    <img src="{{ $product->imagePath }}" alt="...">
+                    <div class="caption">
+                        <h3>{{ $product->title }}</h3>
+                        <p class="description">{{ $product->description }}</p>
+                        <div class="clearfix">
+                        <div class="pull-left price">${{ $product->price }}</div>
+                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                        </div>
+                    </div>
+                    </div>
                 </div>
-            </div>
-            </div>
+            @endforeach
         </div>
-    </div>
+    @endforeach
 @endsection
